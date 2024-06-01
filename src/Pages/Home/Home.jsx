ba08d0202d668526c40ebb4ff437fe1a73686f58
jrazad10 <jrazad10@gmail.com>
2024-06-01 16:14:56 +0600

    updated

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+
+const Home = () => {
+    return (
+        <div>
+            <Helmet>
+                <title>home</title>
+            </Helmet>
+            hello
+        </div>
+    );
+};
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { Helmet } from 'react-helmet-async';
+import AuthProvider from './Providers/AuthProvider';
+import Main from './Layout/Main';
+import Login from './Pages/Login/Login';
+import SignUp from './Pages/SignUp/SignUp';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children:[
+
+    ]
+  },
+  {
+    path: '/login',
+    element: <Login></Login>,
+  },
+  {
+    path: '/signup',
+    element: <SignUp></SignUp>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Helmet>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </Helmet>
   </React.StrictMode>,
 )
