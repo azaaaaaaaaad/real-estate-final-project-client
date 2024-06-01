216a46274a2fd08d802d573c1d7c652db7c077a4
jrazad10 <jrazad10@gmail.com>
2024-06-01 17:46:49 +0600

    BANNER ADDED IN HOMEPAGE

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,26 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
+import img1 from '../../../assets/1.jpg'
+import img2 from '../../../assets/2.jpg'
+import img3 from '../../../assets/3.webp'
+
+const Banner = () => {
+    return (
+        <div>
+             <Carousel>
+                <div>
+                    <img src={img1} />
+                </div>
+                <div>
+                    <img src={img2} />
+                </div>
+                <div>
+                    <img src={img3} />
+                </div>
+            </Carousel>
+        </div>
+    );
+};
+
+export default Banner;
